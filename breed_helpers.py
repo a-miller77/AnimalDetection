db2e739af5dcb61c209f7f57dc67a76d38702aaf
Aiden" "Miller <aiden5077@gmail.com>
2023-06-21 22:54:45 -0500

    added vocab

--- breed_helpers.py
+++ breed_helpers.py
@@ -2,6 +2,21 @@ import xml.etree.ElementTree as ET
 import numpy as np
 from PIL import Image
 
+# +
+def create_vocab(file_path = './data/vocab.txt') -> tuple:
+    with open(file_path) as f:
+        lines = f.readlines()
+        lines = [i[:-1] for i in lines]  # Remove new line characters
+    breed_to_idx = {breed: idx for idx, breed in enumerate(lines)}
+    idx_to_breed = {idx: breed for idx, breed in enumerate(lines)}
+    return breed_to_idx, idx_to_breed
+
+breed_to_idx, idx_to_breed = create_vocab()
+vocab_size = len(breed_to_idx)
+
+
+# -
+
 def get_bbox(annot):
     root = ET.parse(annot).getroot()
     objects = root.findall('object')
@@ -24,3 +39,5 @@ def get_image_path(annot):
 def get_dog_breed(annot):
     file = annot.replace('\\','/').split('/')
     return file[-2].split('-', 1)[-1]
+
+
